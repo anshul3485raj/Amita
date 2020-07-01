ca52474ab565a3be8d87a143506ef0f4fa9342bd
anshul3485raj <59793276+anshul3485raj@users.noreply.github.com>
2020-07-01 12:56:41 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 import tkinter
 import depcal
-import workvidetest
+import vamail
 import test
 import semail
 import graph
@@ -8,27 +8,44 @@ import graph2
 import os
 import threading
 import movie
-import emotionspeach
+class rws():
+    def __init__(self):
+        thread = threading.Thread(target=self.run, args=())
+        thread.start()
+    def run(self):
+        
+        os.system('emotionspeach.pyc')
+        
+       
+class rs():
+    def __init__(self):
+        thread = threading.Thread(target=self.run, args=())
+        thread.start()
+    def run(self):
+        
+        rws()
+        
+        os.system('test.pyc')
+        
 
 class r():
     def __init__(self):
         thread = threading.Thread(target=self.run, args=())
         thread.start()
     def run(self):
+        rs()
         
         os.system('workvidetest.pyc')
-        os.system('emotionspeach.pyc')
-        os.system('test.pyc')
+        
     
 tr=r()
 top = tkinter.Tk()
 top.geometry('430x400')
 def test():
     depcal.test()
-def botg():
-    emotionspeach.chatt()
+
 def mail():
-    semail.sjem()
+    vamail.sem()
 
 def graph0():
     graph2.re()
@@ -38,8 +55,7 @@ def moviek():
   
 w=tkinter.Button(top,text='test ur depression',font='time 14',command=lambda:test())
 w.place(x=0,y=0)
-#w1=tkinter.Button(top,text='chat with amita',font='time 14',command=lambda:botg())
-#w1.place(y=40)
+
 w2=tkinter.Button(top,text='send report ',font='time 14',command=lambda:mail())
 w2.place(y=40)
 w3=tkinter.Button(top,text='see graph',font='time 14',command=lambda:graph0())
